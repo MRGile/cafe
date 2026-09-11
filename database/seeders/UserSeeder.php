d020d9ddc455f3bbf5941c50c7dfee3f0363a020
MRGile <95109775+MRGile@users.noreply.github.com>
2026-09-11 11:42:36 +0700

    Tambah fitur login

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,9 +16,9 @@ class UserSeeder extends Seeder
             'name'       => 'Admin Kedai Kopi',
             'email'      => 'AdminNusantara@gmail.com',
             'password'   => Hash::make('Nusantara123'),
-            // 'role_id'    => $adminRole?->id,
-            // 'created_at' => now(),
-            // 'updated_at' => now(),
+            'role_id'    => $adminRole?->id,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
